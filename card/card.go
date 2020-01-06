097ac5e05c40bae1ae6e8d4676c6bfcedaf55fcf
John George <john@exotel.in>
2020-01-06 23:38:43 +0530

    More to come

---
+++ card/card.go
@@ -0,0 +1,131 @@
+//go:generate stringer -type=Suit,Rank
+package card
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+type Suit uint
+type Rank uint
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+const (
+	_ Rank = iota
+	Ace
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+const (
+	minRank = Ace
+	maxRank = King
+)
+
+var suits = []Suit{Spade, Diamond, Club, Heart}
+
+type Card struct {
+	Suit
+	Rank
+}
+
+func (c Card) String() string {
+	if c.Suit == Joker {
+		return c.Suit.String()
+	}
+	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
+
+func New(opts ...func([]Card) []Card) []Card {
+	var cards []Card
+	for _, s := range suits {
+		for i := minRank; i <= maxRank; i++ {
+			cards = append(cards, Card{
+				Suit: s,
+				Rank: i,
+			})
+		}
+	}
+	for _, opt := range opts {
+		cards = opt(cards)
+	}
+	return cards
+}
+
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return CardRank(cards[i]) < CardRank(cards[j])
+	}
+}
+
+func Sort(less func(cards []Card) func(i, j int) bool) func(cards []Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+func Shuffle(cards []Card) []Card {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	shuffled := make([]Card, len(cards))
+	perm := r.Perm(len(shuffled))
+	for i, shuff := range perm {
+		shuffled[i] = cards[shuff]
+	}
+	return shuffled
+}
+
+func CardRank(c Card) int {
+	r := int(c.Suit)*int(maxRank) + int(c.Rank)
+	return r
+}
+
+func AddJoker(n int) func(cards []Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			j := Card{
+				Suit: Joker,
+				Rank: Rank(i),
+			}
+			cards = append(cards, j)
+		}
+		return cards
+	}
+}
+
+func RemoveCards(cardTypes ...Rank) func(cards []Card) []Card {
+	return func(cards []Card) []Card {
+		for _, cardtype := range cardTypes {
+			cards = Remove(cardtype, cards)
+		}
+		return cards
+	}
+}
+
+func Remove(cardType Rank, cards []Card) []Card {
+
+	return cards
+}
---
+++ card/card_test.go
@@ -0,0 +1,57 @@
+package card
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleCard() {
+	fmt.Println(Card{
+		Suit: Heart,
+		Rank: Ace,
+	})
+	fmt.Println(Card{
+		Suit: Spade,
+		Rank: Two,
+	})
+	//Output:
+	//Ace of Hearts
+	//Two of Spades
+
+}
+
+func TestSort(t *testing.T) {
+	cards := New(DefaultSort)
+	expected := Card{
+		Suit: Spade,
+		Rank: Ace,
+	}
+	if cards[0] != expected {
+		t.Error("Expected ace of spades but got:", cards[0])
+	}
+}
+func TestNewSort(t *testing.T) {
+	cards := New(Sort(Less))
+	expected := Card{
+		Suit: Spade,
+		Rank: Ace,
+	}
+	if cards[0] != expected {
+		t.Error("Expected ace of spades but got:", cards[0])
+	}
+}
+
+func TestAddJoker(t *testing.T) {
+	cards := New(AddJoker(4))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+	fmt.Println(cards)
+	if count != 4 {
+		t.Error("Expected 4 Jokers, received:", count)
+
+	}
+}
